9b24e4a3f2633a3b32d8b3442af91c4a0ede655f
jezykwemgle <96741811+jezykwemgle@users.noreply.github.com>
2023-05-28 18:01:32 +0300

    init database, added create and insert scripts

---
+++ tracks_app/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS tracks;
+
+CREATE TABLE tracks(
+    id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    length INTEGER NOT NULL
+);
---
+++ tracks_app/tracks.sql
@@ -0,0 +1,7 @@
+INSERT INTO tracks (title, artist, genre, length)
+VALUES
+    ('f', 'f', 'f', 10),
+    ('a', 'a', 'a', 10),
+    ('d', 'd', 'd', 10),
+    ('s', 's', 's', 10),
+    ('r', 'r', 'r', 10)
